028b199feb670b27753f367cbc6ada952923ee80
Deepan Raj Kumar <princedeepan@gmail.com>
2020-06-20 13:46:20 +0530

    Removing old file

--- buildServer.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var server = express();
-server.use("/", express.static(__dirname + "/dist/"));
-server.listen(4000, function() {
-  // eslint-disable-next-line no-console
-  console.log("app listening on port http://localhost:4000/");
-});
