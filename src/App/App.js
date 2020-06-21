5c739364005dfc1a091766d2c1f5b3cd6299270f
Deepan Raj Kumar <princedeepan@gmail.com>
2020-06-21 05:41:30 +0530

    adding fonts

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Styled from "styled-components";
+import "./styles.css";
 
-const Title = Styled.h1`
+const Title = Styled.h2`
   font-size: 1.5em;
   text-align: center;
   color: red;
